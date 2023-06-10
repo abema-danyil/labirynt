a017bcf2861a262d65539f18dd3188290cf9305e
Danyil Parkhatskyi <parkhatskyidanyil@gmail.com>
2023-06-10 21:29:38 +0200

    add walls and put the characters in their places

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ clock = pygame.time.Clock()
 
 class GameSprite(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height, image):
+        super().__init__()
         self.rect = pygame.Rect(x, y, width, height)
         self.image = pygame.image.load(file_path(image))
         self.image = pygame.transform.scale(self.image, (width, height))
@@ -28,11 +29,36 @@ class GameSprite(pygame.sprite.Sprite):
     def show(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-player = GameSprite(430, 500, 50, 50, r"images\player.png")
-enemy = GameSprite(350, 400, 30, 30, r"images\enemy.png")
-finish = GameSprite(430, 60, 50, 50, r"images\chest.png")
+player = GameSprite(50, 250, 50, 50, r"images\player.png")
+enemy1 = GameSprite(340, 260, 30, 30, r"images\enemy.png")
+enemy2 = GameSprite(530, 360, 30, 30, r"images\enemy.png")
+enemy3 = GameSprite(800, 460, 30, 30, r"images\enemy.png")
+finish = GameSprite(785, 60, 50, 50, r"images\chest.png")
 
-
+walls = pygame.sprite.Group()
+wall = GameSprite(10, 220, 20, 110, r"images\fall.png")
+wall1 = GameSprite(20, 220, 300, 20, r"images\fall.png")
+wall2 = GameSprite(20, 310, 300, 20, r"images\fall.png")
+wall3 = GameSprite(300, 310, 20, 200, r"images\fall.png")
+wall4 = GameSprite(300, 40, 20, 200, r"images\fall.png")
+wall5 = GameSprite(300, 510, 550, 20, r"images\fall.png")
+wall6 = GameSprite(300, 20, 550, 20, r"images\fall.png")
+wall7 = GameSprite(390, 420, 200, 20, r"images\fall.png")
+wall8 = GameSprite(850, 20, 20, 510, r"images\fall.png")
+wall9 = GameSprite(390, 110, 270, 20, r"images\fall.png")
+wall10 = GameSprite(750, 20, 20, 200, r"images\fall.png")
+wall11 = GameSprite(550, 200, 200, 20, r"images\fall.png")
+wall12 = GameSprite(550, 110, 20, 100, r"images\fall.png")
+wall13 = GameSprite(390, 110, 20, 130, r"images\fall.png")
+wall14 = GameSprite(390, 310, 20, 40, r"images\fall.png")
+wall15 = GameSprite(390, 310, 200, 20, r"images\fall.png")
+wall16 = GameSprite(660, 220, 20, 110, r"images\fall.png")
+wall17 = GameSprite(470, 200, 20, 120, r"images\fall.png")
+wall18 = GameSprite(760, 310, 100, 20, r"images\fall.png")
+wall19 = GameSprite(670, 420, 200, 20, r"images\fall.png")
+wall20 = GameSprite(500, 320, 20, 120, r"images\fall.png")
+walls.add(wall, wall1, wall2, wall3, wall4, wall5, wall6, wall7, wall8
+, wall9, wall10, wall11, wall12, wall13, wall14, wall15, wall16, wall17, wall18, wall19, wall20)
 
 level = 1
 game = True
@@ -45,8 +71,11 @@ while game:
     if level == 1:
         window.blit(fon, (0, 0))
         player.show()
-        enemy.show()
+        enemy1.show()
+        enemy2.show()
+        enemy3.show()
         finish.show()
+        walls.draw(window)
 
     clock.tick(FPS)
     pygame.display.update()
